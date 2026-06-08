c9cee384dfc43ba3d92898de9577ff293942176c
Tahiel1 <luditahiel@gmail.com>
2026-06-08 12:16:43 -0300

    adding player that can move with wasd

---
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private PlayerInputController inputController;
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private float speed;
+
+    private void Start()
+    {
+        inputController = GetComponent<PlayerInputController>();
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        rb.linearVelocityY = (inputController.MoveData.y * speed) * Time.deltaTime;
+        rb.linearVelocityX = (inputController.MoveData.x * speed) * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerInputController.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : MonoBehaviour
+{
+    [Header("Player Actions")]
+    [SerializeField] private Vector2 moveData;
+    [SerializeField] private bool isInteracting;
+    [SerializeField] private bool isChanging;
+
+    private PlayerInput playerActions;
+
+    public Vector2 MoveData => moveData;
+
+    private void Awake()
+    {
+        playerActions = new PlayerInput();
+    }
+
+    private void OnEnable()
+    {
+        playerActions.Player.Enable();
+
+        playerActions.Player.Move.performed += OnMove;
+        playerActions.Player.Move.canceled += OnMove;
+
+        playerActions.Player.Interact.started += OnInteractStart;
+        playerActions.Player.Interact.canceled += OnInteractEnd;
+
+        playerActions.Player.ChangeInteract.performed += OnChange;
+        playerActions.Player.ChangeInteract.canceled += OnChange;
+    }
+
+    private void OnDisable()
+    {
+        playerActions.Player.Move.performed -= OnMove;
+        playerActions.Player.Move.canceled -= OnMove;
+
+        playerActions.Player.Interact.performed -= OnInteractStart;
+        playerActions.Player.Interact.canceled -= OnInteractEnd;
+
+        playerActions.Player.ChangeInteract.performed -= OnChange;
+        playerActions.Player.ChangeInteract.canceled -= OnChange;
+
+        playerActions.Player.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext ctx)
+    {
+        moveData = ctx.ReadValue<Vector2>();
+    }
+    private void OnInteractStart(InputAction.CallbackContext ctx)
+    {
+        isInteracting = true;
+    }
+    private void OnInteractEnd(InputAction.CallbackContext ctx)
+    {
+        isInteracting = false;
+    }
+
+    private void OnChange(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+            isChanging = true;
+        else isChanging = false;
+    }
+}
