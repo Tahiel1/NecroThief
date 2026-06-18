68416ef9f55666091f7f34b6dcd7a330e8b25b9c
Tahiel1 <luditahiel@gmail.com>
2026-06-18 15:31:43 -0300

    creating level 2

--- Assets/Scripts/MapObjects/SafeBox.cs
+++ Assets/Scripts/MapObjects/SafeBox.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -6,25 +7,42 @@ public class SafeBox : MonoBehaviour, ICanThiefTools
     [SerializeField] private int money=50;
     public int Money => money;
     [SerializeField] int successCount;
+    [SerializeField] int maxSuccess = 3;
+
+    [SerializeField] private TextMeshProUGUI remaininSuccessCount;
+
+    private void Awake()
+    {
+        remaininSuccessCount.text = null;
+    }
     public void FailSteal()
     {
         successCount--;
         if(successCount < 0) successCount = 0;
+        ShowSucces();
     }
     public void addSuccess()
     {
         successCount++;
-        if (successCount >= 3) StealMoney();
+        ShowSucces();
+        if (successCount >= maxSuccess) StealMoney();
     }
     public void StealMoney()
     {
         GameController.Instance.AddScore(money);
         money = 0;
+        remaininSuccessCount.text = null;
     }
     public bool IsStealable()
     {
-        bool isStealabel = (money != 0);
-        return isStealabel;
+        bool isStealable = (money != 0);
+        if (isStealable) ShowSucces();
+        return isStealable;
+    }
+    private void ShowSucces()
+    {
+        int succesText = maxSuccess - successCount;
+        remaininSuccessCount.text= succesText.ToString();
     }
 
 }
